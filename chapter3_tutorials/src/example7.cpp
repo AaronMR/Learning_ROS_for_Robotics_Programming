67253416b43af13c73ed5f16f01786dd62609329
enriquefernandez <enrique.fernandez@pal-robotics.com>
2014-02-12 21:29:00 +0100

    uses pcl_conversions instead of pcl

--- chapter3_tutorials/src/example7.cpp
+++ chapter3_tutorials/src/example7.cpp
@@ -4,7 +4,7 @@
 #include <visualization_msgs/Marker.h>
 
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl/ros/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
